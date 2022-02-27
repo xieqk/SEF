2b3f63cc85433411eca3afedf8a2c01bb974ac71
xieqk <xieqk1214@qq.com>
2022-02-27 23:11:17 +0800

    from deep-person-reid repo

---
+++ metrics/__init__.py
@@ -0,0 +1,6 @@
+# modified from https://github.com/KaiyangZhou/deep-person-reid
+from __future__ import absolute_import
+
+from .rank import evaluate_rank
+from .accuracy import accuracy
+from .distance import compute_distance_matrix
